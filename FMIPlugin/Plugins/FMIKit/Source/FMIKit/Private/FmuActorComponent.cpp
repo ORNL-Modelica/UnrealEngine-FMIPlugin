7946182fa44d862f0f435589ccfe0bd053428dd2
Tom Norby <norbytp@ornl.gov>
2020-10-05 16:01:08 -0400

    Skip files that are already unzipped.

--- FMIPlugin/Plugins/FMIKit/Source/FMIKit/Private/FmuActorComponent.cpp
+++ FMIPlugin/Plugins/FMIKit/Source/FMIKit/Private/FmuActorComponent.cpp
@@ -16,7 +16,7 @@ UFmuActorComponent::UFmuActorComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	mPath.FilePath = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() + "../test");
+	mPath.FilePath = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() + "../test.fmu");
 	// ...
 }
 
